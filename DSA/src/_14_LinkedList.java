75d7483e1a3cb29c5208fb86c078a5ceda961d3e
Xavier Brooke <xavier.brooke.official@gmail.com>
2026-08-25 23:02:52 +0530

    practice : palindromeProblem

--- DSA/src/_14_LinkedList.java
+++ DSA/src/_14_LinkedList.java
@@ -43,7 +43,7 @@ public class _14_LinkedList {
         // add
         public void add(int idx, int val) {
             if(idx < 0 || idx > size) {
-                throw  new Error("Index out of bound") ;
+                throw new IndexOutOfBoundsException("Index out of bound");
             }
 
             if(idx == 0) {
@@ -72,19 +72,28 @@ public class _14_LinkedList {
         // removeFirst
         public int removeFirst() {
             if(head == null) {
-                throw new Error("Can't remove element from empty LinkedList") ;
+                throw new IllegalStateException("Can't remove element from empty LinkedList");
             }
 
             int val = head.val ;
             head = head.next ;
             size-- ;
+
+            if(head == null) {
+                tail = null ;
+            }
+
             return val ;
         }
 
         // removeLast
         public int removeLast() {
             if(head == null) {
-                throw new Error("Can't remove element from empty LinkedList") ;
+                throw new IllegalStateException("Can't remove element from empty LinkedList");
+            }
+
+            if(head.next == null) {
+                return removeFirst() ;
             }
 
             Node itrNode = head ;
@@ -104,7 +113,7 @@ public class _14_LinkedList {
         // remove
         public int remove(int idx) {
             if(idx < 0 || idx >= size) {
-                throw new Error("Index out of bound") ;
+                throw new IndexOutOfBoundsException("Index out of bound");
             }
 
             if(idx == 0) {
@@ -142,9 +151,156 @@ public class _14_LinkedList {
             }
             System.out.println("null");
         }
+
+        // linear search (loop)
+        public int search(int key) {
+            if(head == null) {
+                return -1 ;
+            }
+
+            int i = 0 ;
+            Node itrNode = head ;
+            while(itrNode != null) {
+                if(itrNode.val == key) {
+                    return i ;
+                }
+                i++ ;
+                itrNode = itrNode.next ;
+            }
+
+            return -1 ;
+        }
+
+        // linear search (recursive)
+        public int search(Node itrNode, int key) {
+            if(itrNode == null) {
+                return -1 ;
+            }
+
+            if(itrNode.val == key) {
+                return 0 ;
+            }
+
+            int idx = search(itrNode.next, key) ;
+            if(idx == -1) {
+                return -1 ;
+            }
+
+            return idx+1 ;
+        }
+
+        // reverse a linkedList
+        public void reverse() {
+            if(head == null) {
+                throw new IllegalStateException("Can't reverse empty LinkedList") ;
+            }
+
+            if(head == tail) {
+                return ;
+            }
+
+            Node prev = null;
+            Node curr = tail = head ;
+            Node next ;
+            while(curr != null) {
+                next = curr.next ;
+                curr.next = prev ;
+                prev = curr ;
+                curr = next ;
+            }
+
+            head = prev ;
+        }
+
+        // Find & Remove Nth node from End
+        public int remove_Nth_from_end(int n) {
+            if(head == null) {
+                throw new IllegalStateException("Can't remove element from empty LinkedList") ;
+            }
+
+            if(n < 1 || n > size) {
+                throw new IllegalStateException(n + "can't be greater than size, or less than 1") ;
+            }
+
+            if(n == size) {
+                return removeFirst() ;
+            }
+
+            if(n == 1) {
+                return removeLast() ;
+            }
+
+            Node itrNode = head ;
+            int idx = (size - n) ;
+            int i = 1 ;
+            while(i < idx) {
+                itrNode = itrNode.next ;
+                i++ ;
+            }
+
+            int val = itrNode.next.val ;
+            itrNode.next = itrNode.next.next ;
+            size-- ;
+            return val ;
+        }
+
+        // get mid
+        public Node getMid() {
+            if(head == null) {
+                throw new IllegalStateException("Can't get mid node of an empty LinkedList") ;
+            }
+
+            Node slow = head ;
+            Node fast = head ;
+            while((fast != null) && (fast.next != null)) {
+                slow = slow.next ;
+                fast = fast.next.next ;
+            }
+
+            return slow ;
+        }
+
+        // check if a LinkedList is palindrome
+        public boolean isPalindrome() {
+            if(head == null) {
+                throw new IllegalStateException("LinkedList is empty") ;
+            }
+
+            if(head == tail) {
+                return true ;
+            }
+
+            Node prev = getMid() ;
+            Node curr = prev.next ;
+            Node next ;
+            prev.next = null ;
+            while(curr != null) {
+                next = curr.next ;
+                curr.next = prev ;
+                prev = curr ;
+                curr = next ;
+            }
+
+            Node leftPtr = head ;
+            Node rightPtr = prev ;
+            while((rightPtr != null) && (leftPtr != rightPtr)) {
+                if(leftPtr.val != rightPtr.val) {
+                    return false ;
+                }
+                leftPtr = leftPtr.next ;
+                rightPtr =  rightPtr.next ;
+            }
+
+            return true ;
+        }
     }
 
     public static void main(String[] args) {
 
+        LinkedList ll = new LinkedList() ;
+        ll.addFirst(0);
+        ll.addLast(1);
+        ll.addLast(0);
+        System.out.println(ll.isPalindrome());
     }
 }
